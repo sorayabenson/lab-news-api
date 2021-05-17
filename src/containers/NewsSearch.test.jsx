b141dc9801c4cf4c0e06bca8e75d5ccb38e78a6e
Soraya Benson <soraya.benson@gmail.com>
2021-05-17 16:26:58 -0700

    mock test added

--- src/containers/NewsSearch.test.jsx
+++ src/containers/NewsSearch.test.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import NewsSearch from './NewsSearch';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
 require('dotenv').config();
 
+const server = setupServer(
+  rest.get(
+    `https://fauxnewsapi.com/v1/search`,
+    (req, res, ctx) => {
+      const query = req.url.searchParams
+      const q = query.get('ghosts')
+      return res(
+        ctx.json([
+          {
+            title: 'Ghosts, are they you?',
+            author: 'Mr. Boo',
+            description: 'A study on past, present, and future ghosts',
+            url: 'www.ghost.com',
+            source: 'The Washington Ghost'
+          }
+        ])
+      )
+    })
+)
+
 describe('NewsSearch container', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
 
   it('renders App', async () => {
     render(<NewsSearch />);
@@ -17,7 +41,7 @@ describe('NewsSearch container', () => {
     expect(ul).toMatchSnapshot();
 
     const input = await screen.findByLabelText('article search');
-    userEvent.type(input, 'Yikes');
+    userEvent.type(input, 'ghosts');
     expect(input).toMatchSnapshot();
 
     const submitButton = await screen.findByRole('button', { name: 'search-articles' })
